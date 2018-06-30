9e0cbd3009592731191f0422cb9b8dba230e674b
GyuYeong2 <gyheo99@gmail.com>
2018-06-30 13:53:09 +0900

    Vector_Move2

---
+++ gyuyeong/Assets/Scripts/ManualPilot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualPilot : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+    //Update is called once per frame
+    private void Update() {
+     if (Input.GetKey(KeyCode.UpArrow)) {
+         Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+         Vector2 nextPosition = new Vector2(transform.position.x, 5);
+         transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);	
+	} else if (Input.GetKey(KeyCode.DownArrow)) {
+       Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+       Vector2 nextPosition = new Vector2(transform.position.x, -5);
+       transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
+    } else if (Input.GetKey(KeyCode.LeftArrow)) { 
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 nextPosition = new Vector2(-5, transform.position.y);
+        transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
+    } else if(Input.GetKey(KeyCode.RightArrow)) { 
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 nextPosition = new Vector2(5,transform.position.y);
+        transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
+         }
+    }
+  }
