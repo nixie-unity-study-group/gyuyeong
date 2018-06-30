585ff27fe977950db906b95388ca32136a29f152
GyuYeong2 <gyheo99@gmail.com>
2018-06-30 14:51:30 +0900

    lesson_1

--- gyuyeong/Assets/Scripts/AutoPilot.cs
+++ gyuyeong/Assets/Scripts/AutoPilot.cs
@@ -3,30 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoPilot : MonoBehaviour {
-    private bool isMoveRight = true;
-
     // Use this for initialization
     void Start () {
-	}
+
+    }
 
     // Update is called once per frame
     void Update() {
-        if (transform.position.x < 5.0f & isMoveRight) {
-            Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-            Vector2 nextPosition = new Vector2(5, 0);
-            transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
-        } else if (transform.position.x >= 5.0f & isMoveRight) {
-            isMoveRight = false;
-        } else if (transform.position.x <= -5.0f & !isMoveRight)  {
-            isMoveRight = true;
-        } else if (transform.position.x > -5.0f & !isMoveRight) {
-            Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-            Vector2 nextPosition = new Vector2(-5, 0);
-            transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
-        }
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 nextPosition = getNextPosition();
+        transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
+
+    }
 
 
+    Vector2 getNextPosition() { 
+            Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+            if (Input.GetKey(KeyCode.UpArrow)) {
+               pos = new Vector2(transform.position.x, 5);
+            } else if (Input.GetKey(KeyCode.DownArrow)) {
+               pos = new Vector2(transform.position.x, -5);
+            } else if (Input.GetKey(KeyCode.LeftArrow)) {
+               pos = new Vector2(-5, transform.position.y);
+            } else if (Input.GetKey(KeyCode.RightArrow)) {
+               pos = new Vector2(5, transform.position.y);
+           
+         } 
 
-		
+            return pos;      
+        
+     }	
 	}
-}
+
+
+
+
+  
--- gyuyeong/Assets/Scripts/ManualPilot.cs
+++ gyuyeong/Assets/Scripts/ManualPilot.cs
@@ -3,29 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ManualPilot : MonoBehaviour {
+    public float moveVelocity = 0.1f;
+    public float maxRange = 5.0f;
+    private Vector2 currentPosition;
+    private Vector2 nextPosition;
 
     // Use this for initialization
     void Start() {
 
     }
-    //Update is called once per frame
-    private void Update() {
-     if (Input.GetKey(KeyCode.UpArrow)) {
-         Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-         Vector2 nextPosition = new Vector2(transform.position.x, 5);
-         transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);	
-	} else if (Input.GetKey(KeyCode.DownArrow)) {
-       Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-       Vector2 nextPosition = new Vector2(transform.position.x, -5);
-       transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
-    } else if (Input.GetKey(KeyCode.LeftArrow)) { 
-        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-        Vector2 nextPosition = new Vector2(-5, transform.position.y);
-        transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
-    } else if(Input.GetKey(KeyCode.RightArrow)) { 
-        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-        Vector2 nextPosition = new Vector2(5,transform.position.y);
-        transform.position = Vector2.MoveTowards(currentPosition, nextPosition, 0.1f);
-         }
+
+    // Update is called once per frame
+    void Update() {
+         currentPosition = new Vector2(transform.position.x, transform.position.y);
+         nextPosition = getNextPosition();
+         transform.position = Vector2.MoveTowards(currentPosition, nextPosition, moveVelocity);
     }
-  }
+
+
+    Vector2 getNextPosition() {
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            pos = new Vector2(transform.position.x, maxRange);
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            pos = new Vector2(transform.position.x, -maxRange);
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            pos = new Vector2(-maxRange, transform.position.y);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            pos = new Vector2(maxRange, transform.position.y);
+
+        }
+
+        return pos;
+
+     }
+}
